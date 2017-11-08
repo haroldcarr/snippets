89c66de119aa2300ac9f9c3daf3837d8cdfef5e3
Harold Carr <harold.carr@gmail.com>
2017-11-07 21:12:58 -0800

    derive serialize

--- derive-serialize/Code.hs
+++ derive-serialize/Code.hs
@@ -1,11 +1,31 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Code where
 
 import           Data.ByteString
+import           Data.IntervalMap.Generic.Strict
 import           Data.Serialize
 import           Data.Set
 import           GHC.Generics
+
+------------------------------------------------------------------------------
+
+newtype LogIndex = LogIndex Int deriving (Eq, Ord, Generic)
+instance Serialize LogIndex
+
+newtype LogIndexInterval a = LogIndexInterval LogIndex deriving (Eq, Ord, Generic)
+instance Serialize (LogIndexInterval LogIndex)
+
+newtype ActiveAssignment = ActiveAssignment Int deriving (Eq, Ord, Generic)
+instance Serialize ActiveAssignment
+
+data ActiveAssignments
+  = IntervalMap (LogIndexInterval LogIndex) (Set ActiveAssignment)
+  deriving (Eq, Ord, Generic)
+instance Serialize ActiveAssignments
+
+------------------------------------------------------------------------------
 
 data UserTree a
   = Leaf
