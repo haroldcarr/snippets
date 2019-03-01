a95635cad5e9e4ad81ae171957983abcfc120c26
Harold Carr <harold.carr@gmail.com>
2019-03-01 10:03:58 -0700

    added aerqGetQuorum; updated insert error to remove offenders

--- aer-quorum/src/Lib.hs
+++ aer-quorum/src/Lib.hs
@@ -2,8 +2,10 @@
 
 module Lib where
 
-import           Control.Lens
+import           Control.Lens    hiding (transform)
+import qualified Data.List       as L
 import qualified Data.Map.Strict as Map
+import           Data.Ord        (Down (..))
 
 {-# ANN module ("HLint: ignore Eta reduce" :: String) #-}
 
@@ -25,7 +27,8 @@ data    AppendEntriesResponse = AppendEntriesResponse
   } deriving (Show, Eq)
 
 type AERQuorum = Map.Map (LogIndex, Term)
-                         (Map.Map NodeId AppendEntriesResponse)
+                         (Map.Map NodeId
+                                  AppendEntriesResponse)
 
 fakeAER :: Term -> NodeId -> LogIndex -> CumulativeHash -> Bool -> AppendEntriesResponse
 fakeAER t n l c b = AppendEntriesResponse t l n Success Convinced c b
@@ -33,36 +36,78 @@ fakeAER t n l c b = AppendEntriesResponse t l n Success Convinced c b
 fakeCumulativeHash :: String -> CumulativeHash
 fakeCumulativeHash = CumulativeHash
 
-aerqmInsert :: AppendEntriesResponse -> AERQuorum -> Either String AERQuorum
-aerqmInsert aer aerq =
-  case aerqmLookup aer aerq of
+aerqInsertMany :: [AppendEntriesResponse] -> AERQuorum -> Either (String, AERQuorum) AERQuorum
+aerqInsertMany aers m =
+  L.foldl' (\mm aer -> mm >>= aerqInsert aer) (Right m) aers
+
+-- | Returns an error if the given AER is already present but with a different hash.
+-- The error returns the given quorum structure, but with all AERs for the
+-- associated NodeId removed.
+aerqInsert :: AppendEntriesResponse -> AERQuorum -> Either (String, AERQuorum) AERQuorum
+aerqInsert aer aerq =
+  case aerqLookup aer aerq of
     Nothing ->
-      return $ aerqmInsertUnconditional aer aerq
+      return $ aerqInsertUnconditional aer aerq
     Just e  ->
       if _aerHash e == _aerHash aer
       then return aerq
-      else Left (mkAerqmInsertError aer e)
-
-mkAerqmInsertError :: AppendEntriesResponse -> AppendEntriesResponse -> String
-mkAerqmInsertError a1 a2 =
-  "aerqmInsert: AER with different hash; " ++ show a1 ++ "; " ++ show a2
+      else Left (mkAerqInsertError aer e, aerqDeleteAllForNodeId (_aerNodeId e) aerq)
 
 -- https://hackage.haskell.org/package/lens-4.16.1/docs/Control-Lens-Iso.html#v:non
-aerqmInsertUnconditional :: AppendEntriesResponse -> AERQuorum -> AERQuorum
-aerqmInsertUnconditional aer@AppendEntriesResponse {..} aerq =
+aerqInsertUnconditional :: AppendEntriesResponse -> AERQuorum -> AERQuorum
+aerqInsertUnconditional aer@AppendEntriesResponse {..} aerq =
   aerq &  at (_aerIndex, _aerTerm) . non Map.empty
        .  at _aerNodeId
        ?~ aer
 
-aerqmLookup :: AppendEntriesResponse -> AERQuorum -> Maybe AppendEntriesResponse
-aerqmLookup AppendEntriesResponse {..} aerq =
+mkAerqInsertError :: AppendEntriesResponse -> AppendEntriesResponse -> String
+mkAerqInsertError a1 a2 =
+  "aerqmInsert: AER with different hash; " ++ show a1 ++ "; " ++ show a2
+
+aerqLookup :: AppendEntriesResponse -> AERQuorum -> Maybe AppendEntriesResponse
+aerqLookup AppendEntriesResponse {..} aerq =
       Map.lookup (_aerIndex, _aerTerm) aerq
   >>= Map.lookup _aerNodeId
 
-aerqmDeleteAllForNodeId :: NodeId -> AERQuorum -> AERQuorum
-aerqmDeleteAllForNodeId nid aerq =
+aerqDeleteAllForNodeId :: NodeId -> AERQuorum -> AERQuorum
+aerqDeleteAllForNodeId nid aerq =
   Map.foldrWithKey go aerq aerq
  where
   go k m acc = let xx = Map.filterWithKey (\n _ -> n /= nid) m
                 in if null xx then Map.delete k    acc
                               else Map.insert k xx acc
+
+-- | Returns a list of 'AppendEntriesResponse' for the highest possible 'LogIndex'
+-- that has at least @qsize@ aer.
+-- If this returns more than one element, that means there are
+-- different quorums for the same LogIndex different Terms.
+-- That is dealt with by the caller.
+-- If no quorum exists, returns an empty list.
+aerqGetQuorum
+  :: Int
+  -> AERQuorum
+  -> Maybe (LogIndex, [(Term, [AppendEntriesResponse])])
+aerqGetQuorum qsize
+  = transform                            -- transform to output signature
+  . head'                                -- get the largest quorums
+  . L.groupBy cmpLI                      -- group quorums of the same size
+  . L.sortOn (Down . getLI)              -- largest quorums first
+  . filter ((>= qsize) . Map.size . snd) -- get quorums
+  . Map.toList
+ where
+  head'   []  = []
+  head' (x:_) = x
+  getLI       = fst . fst
+  cmpLI x y   = getLI x == getLI y
+  transform ::      [((LogIndex,   Term), Map.Map NodeId AppendEntriesResponse)]
+            -> Maybe  (LogIndex, [(Term,                [AppendEntriesResponse])])
+
+  transform []               = Nothing
+  transform a@(((li,_),_):_) = Just (li, L.foldl' (go li) [] a)
+  go :: LogIndex
+     -> [(Term, [AppendEntriesResponse])]
+     -> ((LogIndex, Term), Map.Map NodeId AppendEntriesResponse)
+     -> [(Term, [AppendEntriesResponse])]
+  go li0 acc ((li, t), m) =
+    if li /= li0 then error "LogIndex do not match" -- TODO : prove not necessary and remove
+    else (t, Map.elems m) : acc
--- aer-quorum/test/AERQSpec.hs
+++ aer-quorum/test/AERQSpec.hs
@@ -9,40 +9,97 @@ import           Lib
 import qualified Data.Map.Strict as Map
 import           Test.Hspec
 
-nid1,nid2 :: NodeId
-nid1  = NodeId 1
-nid2  = NodeId 2
+fAER :: Int -> Int -> Int -> String -> AppendEntriesResponse
+fAER t li n h = fakeAER (Term t) (NodeId n) (LogIndex li) (fakeCumulativeHash h) True
+
+mkAERs :: Int -> Int -> [AppendEntriesResponse]
+mkAERs t li = [ fAER t li n "1" | n <- [ 0.. ] ]
+
 aer1,aer1',aer2 :: AppendEntriesResponse
-aer1  = fakeAER (Term 1) nid1 (LogIndex 1) (fakeCumulativeHash "aer1") True
-aer1' = aer1                   { _aerHash = fakeCumulativeHash "bad" }
-aer2  = fakeAER (Term 1) nid2 (LogIndex 1) (fakeCumulativeHash "aer2") True
+aer1  = fAER 1 1 1 "aer1"
+aer1' = aer1 { _aerHash = fakeCumulativeHash "bad" }
+aer2  = fAER 1 1 2 "aer2"
 
 spec :: Spec
-spec = do
-  liars
-  describe "AERQuorum" $ do
-    it "aerqmInsert same AER twice" $ do
-      aerqmInsert aer1 Map.empty >>= aerqmInsert aer1
-      `shouldBe`
-      aerqmInsert aer1 Map.empty
+spec  = do
+  honestInsert
+  dishonestInsert
+  getQuorum
+
+getQuorum :: Spec
+getQuorum  = describe "getQuorum" $ do
+  let xT1LI1 = mkAERs 1 1
+      xT1LI2 = mkAERs 1 2
+      xT2LI1 = mkAERs 2 1
+      xT2LI2 = mkAERs 2 2
+
+  it "no quorum" $
+    aerqGetQuorum 3 <$> aerqInsertMany
+                        (take 1 xT1LI1 ++ take 1 xT1LI2 ++ take 2 xT2LI1 ++ take 2 xT2LI2)
+                        Map.empty
+    `shouldBe`
+    Right Nothing
+
+  it "quorum" $
+    aerqGetQuorum 3 <$> aerqInsertMany
+                        (take 1 xT1LI1 ++ take 1 xT1LI2 ++ take 2 xT2LI1 ++ take 3 xT2LI2)
+                        Map.empty
+    `shouldBe`
+    Right (Just (LogIndex 2,[(Term 2, take 3 xT2LI2)]))
+
+  it "quorum >" $
+    aerqGetQuorum 5 <$> aerqInsertMany (take 4 xT1LI1) Map.empty
+    `shouldBe`
+    Right Nothing
+
+  it "quorum --" $
+    aerqGetQuorum 4 <$> aerqInsertMany (take 4 xT1LI1) Map.empty
+    `shouldBe`
+    Right (Just (LogIndex 1,[(Term 1, take 4 xT1LI1)]))
+
+  it "quorum <" $
+    aerqGetQuorum 3 <$> aerqInsertMany (take 4 xT1LI1) Map.empty
+    `shouldBe`
+    Right (Just (LogIndex 1,[(Term 1, take 4 xT1LI1)]))
+
+  it "two quorums for same LogIndex but different Terms" $
+    aerqGetQuorum 3 <$> aerqInsertMany (take 4 xT1LI1 ++ take 4 xT2LI1) Map.empty
+    `shouldBe`
+    Right (Just (LogIndex 1,[(Term 2, take 4 xT2LI1)
+                            ,(Term 1, take 4 xT1LI1)]))
+
+  it "two quorums, ignore non-quorum" $
+    aerqGetQuorum 3 <$> aerqInsertMany (take 4 xT1LI1 ++ take 4 xT2LI1 ++ take 2 xT2LI2) Map.empty
+    `shouldBe`
+    Right (Just (LogIndex 1,[(Term 2, take 4 xT2LI1)
+                            ,(Term 1, take 4 xT1LI1)]))
+
+honestInsert :: Spec
+honestInsert  = describe "honestInsert" $
+  it "aerqInsert same AER twice" $
+    (aerqInsert aer1 Map.empty >>= aerqInsert aer1)
+    `shouldBe`
+    aerqInsert aer1 Map.empty
 
 -- if someone lies
 -- - they should have no influence (so they should be completely removed)
 -- - they should not be able to interfere with honest ones
-liars :: Spec
-liars = describe "liars" $ do
-  it "insert good after: aerqmInsert same AER twice - but with different CumulativeHash-s" $ do
-    aerqmInsert aer1 Map.empty >>= aerqmInsert aer1' >>= aerqmInsert aer2
+dishonestInsert :: Spec
+dishonestInsert  = describe "dishonestInsert" $ do
+  it "insert good after: aerqInsert same AER twice - but with different CumulativeHash-s" $
+    (aerqInsert aer1 Map.empty >>= aerqInsert aer1' >>= aerqInsert aer2)
     `shouldBe`
-    Left (mkAerqmInsertError aer1' aer1)
+    Left (mkAerqInsertError aer1' aer1, Map.empty)
 
-  it "insert good middle: aerqmInsert same AER twice - but with different CumulativeHash-s" $ do
-    aerqmInsert aer1 Map.empty >>= aerqmInsert aer2  >>= aerqmInsert aer1'
-    `shouldBe`
-    Left (mkAerqmInsertError aer1' aer1)
+  it "insert good middle: aerqInsert same AER twice - but with different CumulativeHash-s" $
+    let Right xx = aerqInsert aer2 Map.empty
+     in (aerqInsert aer1 Map.empty >>= aerqInsert aer2 >>= aerqInsert aer1')
+        `shouldBe`
+        Left (mkAerqInsertError aer1' aer1, xx)
 
-  it "insert good before: aerqmInsert same AER twice - but with different CumulativeHash-s" $ do
-    aerqmInsert aer2 Map.empty >>= aerqmInsert aer1  >>= aerqmInsert aer1'
-    `shouldBe`
-    Left (mkAerqmInsertError aer1' aer1)
+  it "insert good before: aerqInsert same AER twice - but with different CumulativeHash-s" $
+    let Right xx = aerqInsert aer2 Map.empty
+     in (aerqInsert aer2 Map.empty >>= aerqInsert aer1 >>= aerqInsert aer1')
+        `shouldBe`
+        Left (mkAerqInsertError aer1' aer1, xx)
 
